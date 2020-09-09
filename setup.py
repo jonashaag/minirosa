d19cfbb1dea339dba6e2a337ec910097b4e8d694
Jonas Haag <jonas@lophus.org>
2020-09-09 12:32:50 +0200

    Minirosa

--- setup.py
+++ setup.py
@@ -3,20 +3,20 @@ import sys
 
 from importlib.machinery import SourceFileLoader
 
-version = SourceFileLoader('librosa.version',
-                           'librosa/version.py').load_module()
+version = SourceFileLoader('minirosa.version',
+                           'minirosa/version.py').load_module()
 
 with open('README.md', 'r') as fdesc:
     long_description = fdesc.read()
 
 setup(
-    name='librosa',
+    name='minirosa',
     version=version.version,
     description='Python module for audio and music processing',
     author='Brian McFee',
     author_email='brian.mcfee@nyu.edu',
     url='https://librosa.org',
-    download_url='https://github.com/librosa/librosa/releases',
+    download_url='https://github.com/jonashaag/minirosa',
     packages=find_packages(),
     package_data={'': ['example_data/*']},
     long_description=long_description,
@@ -36,19 +36,21 @@ setup(
     keywords='audio music sound',
     license='ISC',
     install_requires=[
-        'audioread >= 2.0.0',
         'numpy >= 1.15.0',
-        'scipy >= 1.0.0',
-        'scikit-learn >= 0.14.0, != 0.19.0',
-        'joblib >= 0.14',
-        'decorator >= 3.0.0',
-        'resampy >= 0.2.2',
-        'numba >= 0.43.0',
-        'soundfile >= 0.9.0',
-        'pooch >= 1.0'
     ],
     python_requires='>=3.6',
     extras_require={
+        'all': [
+            'audioread >= 2.0.0',
+            'scipy >= 1.0.0',
+            'scikit-learn >= 0.14.0, != 0.19.0',
+            'joblib >= 0.14',
+            'decorator >= 3.0.0',
+            'resampy >= 0.2.2',
+            'numba >= 0.43.0',
+            'soundfile >= 0.9.0',
+            'pooch >= 1.0',
+        ],
         'docs': ['numpydoc', 'sphinx!=1.3.1', 'sphinx_rtd_theme==0.5.*',
                  'numba < 0.50',
                  'matplotlib >= 2.0.0, < 3.3',
